6071355c918dad0ffa9df11f15d9b51d2977301a
Alexbejen <alexbejen24@gmail.com>
2025-01-26 17:27:08 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
 function steam_Sensor () {
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
-        return 0
-    } else {
         return 1
+    } else {
+        return 0
     }
 }
 basic.forever(function () {
     steam_Sensor()
-    if (steam_Sensor() == 0) {
+    if (steam_Sensor() == 1) {
         music.setVolume(255)
         music.ringTone(262)
+        music.setVolume(0)
     } else {
         music.setVolume(0)
     }
